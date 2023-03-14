f2b1b0f615d0db3fb9302ebb17a48a5665b82f35
Rimo <sampriyoguin@gmail.com>
2023-03-15 01:23:20 +0530

    Changed spacings

--- DAA/strassen.c
+++ DAA/strassen.c
@@ -37,9 +37,9 @@ void matrix_print(const char *str, int **a, int row){
 
     for(int i = 0; i < row; i++){
         if(i == 0)
-            w = printf("%s = ", str);
+            w = printf("%s =", str);
         else
-            printf("%*s ", w, "");
+            printf("%*s", w, "");
         for(int j = 0; j < row; j++){
             printf("%*d ", nw, a[i][j]);
         }
@@ -47,6 +47,7 @@ void matrix_print(const char *str, int **a, int row){
     }
     fflush(stdout);
 }
+
 
 int **matrix_add(int **a, int **b, int row, int deallocate){
     int **c = matrix_allocate(row, row);
