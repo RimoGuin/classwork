7cf41750e19ce090cb170feaf33d2e16230ecfc6
Rimo <sampriyoguin@gmail.com>
2023-05-23 07:20:16 +0530

    Added algos

--- DAA/Graph Algorithms/Shortest path/floyd-warshall-pred.c
+++ DAA/Graph Algorithms/Shortest path/floyd-warshall-pred.c
@@ -1,3 +1,14 @@
+/*
+ * FLOYD-WARSHALL'(W)
+ *  n = W.rows
+ *	D = W
+ *  for k = 1 to n
+ *      for i = 1 to n
+ *          for j = 1 to n
+ *              d[i, j] = min(d[i, j], d[i, k] + d[k, j])
+ *	return D
+ */
+
 #include <stdio.h>
 
 #define MAX_V 10 // Maximum number of vertices in the graph
