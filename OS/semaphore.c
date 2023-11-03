0127d37db5c4f0cf3cfc7a231558b5c842023091
rimo <sampriyoguin@gmail.com>
2023-11-03 06:25:10 +0530

    Added semaphore programs

---
+++ OS/semaphore.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<pthread.h>
+#include<semaphore.h>
+
+void *thread_function(void *arg);
+sem_t bin_sem;
+#define WORK_SIZE 1024
+char WORK_AREA[WORK_SIZE];
+
+int main() {
+	int res;
+	pthread_t a_thread;
+	void *thread_result;
+	res = sem_init(&bin_sem, 0, 0);
+	if(res != 0) {
+		perror("Semaphore initiation failed.");
+		exit(EXIT_FAILURE);
+	}
+	printf("Input some text. Enter 'end' to finish.\n");
+	while(strncmp("end", WORK_AREA, 3) != 0) {
+		fgets(WORK_AREA, WORK_SIZE, stdin);
+		sem_post(&bin_sem);
+	}
+
+	printf("\n Waiting for thread to finish...\n");
+	res = pthread_join(a_thread, &thread_result);
+	if(res != 0) {
+		perror("Thread join failed");
+		exit(EXIT_FAILURE);
+	}
+	printf("Thread joined\n");
+	sem_destroy(&bin_sem);
+	exit(EXIT_SUCCESS);
+}
+
+void *thread_function(void *arg) {
+	sem_wait(&bin_sem);
+	while(strncmp("end", WORK_AREA, 3) != 0) {
+		printf("You input %ld characters \n", strlen(WORK_AREA) - 1);
+		sem_wait(&bin_sem);
+	}
+	pthread_exit(NULL);
+}
